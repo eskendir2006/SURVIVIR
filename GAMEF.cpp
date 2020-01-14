ff3275fb3a0a0cbababe843fcd9ef52161abc2fd
eskendir2006 <59607469+eskendir2006@users.noreply.github.com>
2020-01-14 21:31:33 +0600

    upd
    Project Started!

---
+++ GAMEF.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include <cstdlib>
+#include <chrono>
+
+#define sleep this_thread::sleep_for(chrono::milliseconds(1000)) 
+
+using namespace std;
+
+int main(){
+	
+	cout<<"SURVIVOR";
+	sleep;
+	
+}
+/*
+
+0=Black
+1=Blue
+2=Green
+3=Aqua
+4=Red
+5=Purple
+6=Yellow
+7=White
+8=Gray
+9=Light Blue
+A=Light Green
+B=Light Aqua
+C=Light Red
+D=Light Purple
+E=Light Yellow
+F=Bright White
+*/
+
